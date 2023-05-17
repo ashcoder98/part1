b0bdbe3a760fcb2ab196634cacd4e8e89a377f54
Alex Harris <alexharrisemail@gmail.com>
2023-05-16 22:31:44 -0400

    exercise 1.2

--- exercise1.1/src/App.js
+++ exercise1.1/src/App.js
@@ -5,12 +5,21 @@ const Header = (props) => {
     </>
   )
 }
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
     <p>{props.part1} {props.exercises1} </p>
     <p>{props.part2} {props.exercises2} </p>
     <p>{props.part3} {props.exercises3} </p>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+    <Part part1={props.part1} exercises1={props.exercises1}/>
+    <Part part2={props.part2} exercises2={props.exercises2}/>
+    <Part part3={props.part3} exercises3={props.exercises3}/>
     </>
   )
 }
@@ -36,9 +45,9 @@ const App = () => {
     <div>
       <Header course={course}/>
       
-       <Content part1={part1} exercises1={exercises1} />
-       <Content part2={part2} exercises2={exercises2} />
-       <Content part3={part3} exercises3={exercises3} />
+       <Content part1='Fundamentals of React' exercises1={exercises1} 
+       part2='Using props to pass data' exercises2={exercises2}
+       part3='State of a component' exercises3={exercises3}/>
         <Total  total={exercises1 + exercises2 + exercises3} />
     </div>
   )
