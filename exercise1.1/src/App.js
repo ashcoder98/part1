9c45b480fef6b4a7dd34ab190028fd0b73081fdc
Alex Harris <alexharrisemail@gmail.com>
2023-05-17 21:11:22 -0400

    utilizes variables

--- exercise1.1/src/App.js
+++ exercise1.1/src/App.js
@@ -45,9 +45,9 @@ const App = () => {
     <div>
       <Header course={course}/>
       
-       <Content part1='Fundamentals of React' exercises1={exercises1} 
-       part2='Using props to pass data' exercises2={exercises2}
-       part3='State of a component' exercises3={exercises3}/>
+       <Content part1={part1} exercises1={exercises1} 
+       part2={part2} exercises2={exercises2}
+       part3={part3} exercises3={exercises3}/>
         <Total  total={exercises1 + exercises2 + exercises3} />
     </div>
   )
